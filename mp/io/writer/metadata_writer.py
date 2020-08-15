2a15271bc723104bfa5cb0d93d56ff744879d8f3
Edward Bridges <github@eqbridges.com>
2020-08-15 16:13:11 -0400

    refactor: introduce logic for inserting vs updating metadata

--- mp/io/writer/metadata_writer.py
+++ mp/io/writer/metadata_writer.py
@@ -1,3 +1,6 @@
+from logging import info, debug, error
+from sys import stdout
+
 from mp.io.writer.connection_factory import ConnectionFactory
 
 
@@ -12,6 +15,19 @@ class FilehandleMetadataWriter(MetadataWriter):
             self.output = output
             self.formatter = formatter
 
+    def __enter__(self):
+        debug('FilehandleMetadataWriter: entering context')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        debug('FilehandleMetadataWriter: exiting context')
+        self.output.flush()
+        if not self.output == stdout:
+            info('not closing output because it is stdout')
+            self.output.close()
+        if exc_val is not None:
+            error(f'exception {exc_type} when closing file handle: {exc_val}\n{exc_tb}')
+
     def write(self, metadata):
         self.output.write(self.formatter(metadata))
         self.output.flush()
@@ -22,14 +38,33 @@ class DatabaseMetadataWriter(MetadataWriter):  # pragma: no cover
         self.connection_factory = connection_factory
 
     def __enter__(self):
-        self.connection = self.connection_factory.connect()
+        cf = self.connection_factory
+        self.connection = cf.connect()
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
+        if exc_val is not None:
+            error(f'exception {exc_type} when closing db handle: {exc_val}\n{exc_tb}')
+
+    def write(self, metadata):
+        if self.exists(metadata.file_path):
+            info(f'file_path {metadata.file_path} already exists in db, updating it.')
+            self.update(metadata)
+            return metadata.id
+        else:
+            info(f'inserting new file_path {metadata.file_path}.')
+            id = self.insert(metadata)
+            return id
 
     def exists(self, path):
         pass
 
-    def write(self, metadata):
-        # not yet implemented
+    def insert(self, metadata):
+        pass
+
+    def update(self, metadata):
+        pass
+
+    def delete(self, image_key):
         pass
