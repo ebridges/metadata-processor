d07d5539c2451334d9ac654167a940938d5bcd85
Edward Bridges <github@eqbridges.com>
2020-08-16 17:42:41 -0400

    chore: update mock classes to support addl test cases

--- tests/mp/io/writer/mock_connection_factory.py
+++ tests/mp/io/writer/mock_connection_factory.py
@@ -1,12 +1,34 @@
 from mp.io.writer.connection_factory import ConnectionFactory
 
 
-class MockConnection:
+class MockCursor:
     def __init__(self):
         self.close_count = 0
 
     def close(self):
         self.close_count = self.close_count + 1
+
+
+class MockConnection:
+    def __init__(self):
+        self.close_count = 0
+        self.cursor_count = 0
+        self.commit_count = 0
+        self.rollback_count = 0
+        self.mock_cursor = MockCursor()
+
+    def close(self):
+        self.close_count = self.close_count + 1
+
+    def cursor(self):
+        self.cursor_count = self.cursor_count + 1
+        return self.mock_cursor
+
+    def commit(self):
+        self.commit_count = self.commit_count + 1
+
+    def rollback(self):
+        self.rollback_count = self.rollback_count + 1
 
 
 class MockConnectionFactory(ConnectionFactory):
