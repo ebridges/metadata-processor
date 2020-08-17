ea11355d5f121d5557c75320105fd68de26f76d9
Edward Bridges <github@eqbridges.com>
2020-08-17 06:38:33 -0400

    chore: removed unused classes

--- mp/io/loader/filesystem_loader.py
+++
@@ -1,6 +0,0 @@
-class FilesystemLoader:
-    def __init__(self, path):
-        self.path = path
-
-    def file_path(self):
-        return self.path
--- tests/mp/io/loader/test_filesystem_loader.py
+++
@@ -1,8 +0,0 @@
-from mp.io.loader.filesystem_loader import FilesystemLoader
-
-
-def test_filesystemloader():
-    expected = '/fake/file/path'
-    under_test = FilesystemLoader(expected)
-    actual = under_test.file_path()
-    assert expected == actual
