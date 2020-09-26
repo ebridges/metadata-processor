c7059d8780957cd159c7594581bfa54e13145b85
Edward Bridges <github@eqbridges.com>
2020-09-26 08:16:56 -0400

    refactor: add support for side effects

--- tests/mp/io/writer/mock_metadata_writer.py
+++ tests/mp/io/writer/mock_metadata_writer.py
@@ -9,6 +9,7 @@ class MockDatabaseMetadataWriter(DatabaseMetadataWriter):
         insert_retval=uuid4(),
         update_retval=uuid4(),
         delete_retval=1,
+        side_effects={}
     ):
         self.enter_count = 0
         self.exit_count = 0
@@ -20,6 +21,7 @@ class MockDatabaseMetadataWriter(DatabaseMetadataWriter):
         self.update_retval = update_retval
         self.delete_count = 0
         self.delete_retval = delete_retval
+        self.side_effects = side_effects
 
     def __enter__(self):
         self.enter_count = self.enter_count + 1
@@ -30,16 +32,24 @@ class MockDatabaseMetadataWriter(DatabaseMetadataWriter):
 
     def exists(self, path):
         self.exists_count = self.exists_count + 1
+        if 'exists' in self.side_effects:
+            raise self.side_effects['exists']
         return self.exists_retval
 
     def insert(self, metadata):
         self.insert_count = self.insert_count + 1
+        if 'insert' in self.side_effects:
+            raise self.side_effects['insert']
         return self.insert_retval
 
     def update(self, metadata):
         self.update_count = self.update_count + 1
+        if 'update' in self.side_effects:
+            raise self.side_effects['update']
         return self.update_retval
 
     def delete(self, image_key):
         self.delete_count = self.delete_count + 1
+        if 'delete' in self.side_effects:
+            raise self.side_effects['delete']
         return self.delete_retval
